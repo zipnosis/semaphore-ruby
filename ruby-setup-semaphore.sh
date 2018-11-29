7d9db2e9acbea5b4b049d36751cf94d0f5f68796
Jake Good <jake.good@zipnosis.com>
2018-11-29 13:21:31 -0600

    Try this semaphore script

--- ruby-setup-semaphore.sh
+++ ruby-setup-semaphore.sh
@@ -1,43 +1,11 @@
 #! /usr/bin/env bash
 
-. /home/runner/.bash_profile
+ruby_version=$(head -n 3 Gemfile | grep -o '[0-9]\.[0-9]\.[0-9]')
+cd ~/.rbenv/plugins/ruby-build && git pull && cd $SEMAPHORE_PROJECT_DIR
+rbenv install $ruby_version -s
+rbenv local $ruby_version
+gem install bundler --no-ri --no-rdoc
 
-ruby_version=${1:-"2.5.3"}
-gem_version=${2:-"2.7.6"}
-ruby_archive="$ruby_version.tar.gz"
-ruby_install_path="/home/runner/.rbenv/versions/$ruby_version"
-
-echo "*****************************************"
-echo "Setting up Ruby $ruby_version"
-echo "*****************************************"
-
-if ! [ -d $ruby_install_path ]; then
-  if ! [ -e $SEMAPHORE_CACHE_DIR/$ruby_archive ]; then
-    cd /home/runner/.rbenv/plugins/ruby-build && git pull && cd -
-    CFLAGS='-fPIC' rbenv install $ruby_version
-    (cd $SEMAPHORE_CACHE_DIR && tar -cf $ruby_archive -C $ruby_install_path .)
-  else
-    echo "Ruby $ruby_version installation archive found in cache. Unpacking..."
-    (cd $SEMAPHORE_CACHE_DIR && mkdir $ruby_install_path && tar xf $ruby_archive -C $ruby_install_path)
-  fi
-else
-  echo "Ruby $ruby_version already installed. Skipping setup..."
-fi
-
-echo "Activating Ruby $ruby_version"
-rbenv global $ruby_version
-
-if ! [ $gem_version = "$(gem --version)" ]; then
-  echo "Updating RubyGems to version $gem_version"
-  gem update --system $gem_version --no-ri --no-rdoc
-fi
-
-echo "Installing bundler..."
-yes | gem install bundler --no-ri --no-rdoc
-
-echo "Setup complete."
-echo "-----------------------------------------------"
-echo "Details:"
-echo "$(ruby --version)"
-echo "$(bundle --version)"
-echo "RubyGems version: $(gem --version)"
+echo "Using ruby $(ruby --version)"
+echo "Using gem $(gem --version)"
+echo "Using bundler $(bundler --version)"
