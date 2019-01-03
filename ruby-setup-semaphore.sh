da25c57d0b8c56077d1c976681edff4fd13c93c8
Jake Good <jake.good@zipnosis.com>
2019-01-03 11:27:09 -0600

    Make sure we're using bundler 1.17.3

--- ruby-setup-semaphore.sh
+++ ruby-setup-semaphore.sh
@@ -4,7 +4,7 @@ ruby_version=$(head -n 3 Gemfile | grep -o '[0-9]\.[0-9]\.[0-9]')
 cd ~/.rbenv/plugins/ruby-build && git pull && cd $SEMAPHORE_PROJECT_DIR
 rbenv install $ruby_version -s
 rbenv local $ruby_version
-gem install bundler --no-ri --no-rdoc
+gem install bundler -v 1.17.3 --no-ri --no-rdoc
 
 echo "Using ruby $(ruby --version)"
 echo "Using gem $(gem --version)"
